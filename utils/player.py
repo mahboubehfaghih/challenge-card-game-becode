43a2ddcea2ddedd6720d4d84791bbbb8d61d4566
mahboubehfaghih <98815460+mahboubehfaghih@users.noreply.github.com>
2022-02-23 14:00:54 +0100

    Update player.py

--- utils/player.py
+++ utils/player.py
@@ -1,13 +1,19 @@
 import random
+
 class Deck:
     def __init__(self):
         self.deck = []
         for suit in range(4):
             for rank in range(13):
-                self.deck.append(Card(suit, rank))
+              if suit == '\u2666' or suit=='\u2665' :
+                   self.deck.append(Card(suit, rank, "red"))
+              else :
+                   self.deck.append(Card(suit, rank, "black" ))
+               #for color in ["red" , "black" ]:
+                 # self.deck.append(Card(suit, rank, color))
         self.shuffle()
         
-    def __len__(self):
+    def __len__(self):      #the number of card
         return len(self.deck)
     
     def add_card(self, card):
@@ -17,4 +23,13 @@ class Deck:
         return self.deck.pop()
     
     def shuffle(self):
-        random.shuffle(self.deck)
+        random.shuffle(self.deck)    
+
+
+#deck= Deck()
+#card1= Card(1, 1, 0)
+#deck.add_card(card1)
+#deck.shuffle()
+#print(deck.pop_card())
+#print(len(deck ))
+
